c05881444012dba8e17605ffa50e3dc5b6916955
menotte <139005962+menotte@users.noreply.github.com>
2023-07-09 20:26:41 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import json
+import re
 
 def search(query):
     command = f"findstr /S /I /C:'{query}' database/*"
@@ -15,22 +16,26 @@ def get_uuid(username):
             return data["results"][0]["uuid"]
     return None
 
+def get_usernames(uuid):
+    command = f'curl -s "https://laby.net/api/user/{uuid}/get-names"'
+    response = os.popen(command).read()
+    if response:
+        data = json.loads(response)
+        if isinstance(data, list) and len(data) > 0:
+            return [name["name"] for name in data]
+    return []
+
 def get_previous_usernames(username):
     uuid = get_uuid(username)
     if uuid is not None:
-        command = f'curl -s "https://laby.net/api/user/{uuid}/get-names"'
-        response = os.popen(command).read()
-        if response:
-            data = json.loads(response)
-            if isinstance(data, list) and len(data) > 1:
-                print(f"Previous usernames for {username}:")
-                for name in data[:-1]:
-                    print(name["name"])
-                    search(name["name"])
-            else:
-                print("No previous usernames found.")
+        usernames = get_usernames(uuid)
+        if len(usernames) > 0:
+            print(f"Usernames for {username}:")
+            for name in usernames:
+                print(name)
+                search(name)
         else:
-            print("Failed to retrieve previous usernames.")
+            print("No usernames found for the user.")
     else:
         print("No UUID found for the username.")
 
@@ -44,17 +49,17 @@ def france_search(query):
     file_paths = glob.glob("france/**", recursive=True)
     for file_path in file_paths:
         if os.path.isfile(file_path) and file_path.lower().endswith(extensions):
-            with open(file_path, "r", encoding="utf-8") as file:
-                try:
+            try:
+                with open(file_path, "r", encoding="utf-8") as file:
                     content = file.read()
-                    if query in content:
-                        print(f"Found in {file_path}:")
-                        print(content)
-                        print()
-                        break  # Stop searching other files once the exact match is found
-
-                except Exception as e:
-                    print(f"Error occurred while reading {file_path}: {e}")
+                    lines = content.splitlines()
+                    for line in lines:
+                        if query.lower() in line.lower():
+                            print(f"Found in {file_path}:")
+                            print(line)
+                            print()
+            except Exception as e:
+                print(f"Error occurred while reading {file_path}: {e}")
 
 while True:
     command = input("Enter a command (search, final, wak, fb): ")
