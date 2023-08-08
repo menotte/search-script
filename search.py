ac385e614618cfa2151f5e932bcf0819054bee3b
menotte <139005962+menotte@users.noreply.github.com>
2023-08-08 21:15:24 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,93 +1,75 @@
 import os
-import glob
 import json
-import re
 from colorama import Fore, Style
+import subprocess
 
-def search(query):
-    command = f"findstr /S /I /C:'{query}' database/*"
-    os.system(command)
+def search(query, directory):
+    command = ["rg", "-i", query, directory]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = process.communicate()
 
-def get_uuid(username):
+    if stdout:
+        print(stdout.decode("utf-8"))
+    elif stderr:
+        print(stderr.decode("utf-8"))
+    else:
+        print("No results found.")
+
+def get_previous_usernames(username):
     command = f'curl -s "https://laby.net/api/search/names/{username}"'
     response = os.popen(command).read()
     if response:
         data = json.loads(response)
         if "results" in data and len(data["results"]) > 0:
-            return data["results"][0]["uuid"]
-    return None
-
-def get_usernames(uuid):
-    command = f'curl -s "https://laby.net/api/user/{uuid}/get-names"'
-    response = os.popen(command).read()
-    if response:
-        data = json.loads(response)
-        if isinstance(data, list) and len(data) > 0:
-            return [name["name"] for name in data]
-    return []
-
-def get_previous_usernames(username):
-    uuid = get_uuid(username)
-    if uuid is not None:
-        usernames = get_usernames(uuid)
-        if len(usernames) > 0:
-            print(f"{Fore.RED}Usernames for {username}:{Style.RESET_ALL}")
-            for name in usernames:
-                print(f"{Fore.RED}Username: {name}{Style.RESET_ALL}")
-                search(name)
+            uuid = data["results"][0]["uuid"]
+            command = f'curl -s "https://laby.net/api/user/{uuid}/get-names"'
+            response = os.popen(command).read()
+            if response:
+                usernames = [name["name"] for name in json.loads(response)]
+                if len(usernames) > 0:
+                    print(f"{Fore.RED}Usernames for {username}:{Style.RESET_ALL}")
+                    for name in usernames:
+                        print(f"{Fore.RED}Username:{Style.RESET_ALL} {name}")
+                        search(name, "database/")
+                else:
+                    print("No usernames found for the user.")
+            else:
+                print("Failed to retrieve previous usernames.")
         else:
-            print("No usernames found for the user.")
+            print("No UUID found for the username.")
     else:
-        print("No UUID found for the username.")
+        print("Failed to retrieve UUID for the username.")
 
 def wakanim_search(query):
-    command = f"findstr /S /I /C:'{query}' wakanim/*"
-    os.system(command)
+    search(query, "wakanim/")
 
 def france_search(query):
-    extensions = (".txt", ".json", ".sql")
-    print(f"Searching in France directory for '{query}':")
-    file_paths = glob.glob("france/**", recursive=True)
-    for file_path in file_paths:
-        if os.path.isfile(file_path) and file_path.lower().endswith(extensions):
-            try:
-                with open(file_path, "r", encoding="utf-8") as file:
-                    content = file.read()
-                    lines = content.splitlines()
-                    for line in lines:
-                        if query.lower() in line.lower():
-                            line = line.replace(query, f"{Fore.RED}{query}{Style.RESET_ALL}")
-                            print(f"Found in {file_path}:")
-                            print(f"Username: {line}")
-                            print()
-            except Exception as e:
-                print(f"Error occurred while reading {file_path}: {e}")
+    search(query, "france/")
 
-title = r'''
-   _____                     _     _____  ____  
-  / ____|                   | |   |  __ \|  _ \ 
- | (___   ___  __ _ _ __ ___| |__ | |  | | |_) |
-  \___ \ / _ \/ _` | '__/ __| '_ \| |  | |  _ < 
-  ____) |  __/ (_| | | | (__| | | | |__| | |_) |
- |_____/ \___|\__,_|_|  \___|_| |_|_____/|____/ 
-                                                
-                                                '''
-
-print(f"{Fore.GREEN}{title.center(50)}{Style.RESET_ALL}")
-print()
+title = """
+   _____                     _     
+  / ____|                   | |    
+ | (___   ___  __ _ _ __ ___| |__  
+  \___ \ / _ \/ _` | '__/ __| '_ \ 
+  ____) |  __/ (_| | | | (__| | | |
+ |_____/ \___|\__,_|_|  \___|_| |_|
+                                   
+                                   
+"""
 
 while True:
+    print(f"{Fore.GREEN}{title}{Style.RESET_ALL}\n")
     print(f"[{Fore.GREEN}1{Style.RESET_ALL}] search")
     print(f"[{Fore.GREEN}2{Style.RESET_ALL}] final")
     print(f"[{Fore.GREEN}3{Style.RESET_ALL}] wakanim")
     print(f"[{Fore.GREEN}4{Style.RESET_ALL}] facebook")
     print(f"[{Fore.GREEN}5{Style.RESET_ALL}] exit")
 
-    command = input(f"\n{Fore.GREEN}Command number: {Style.RESET_ALL}")
+    command = input(f"\n{Fore.GREEN}Enter the command number:{Style.RESET_ALL} ")
 
     if command == "1":
         query = input(f"{Fore.RED}Enter search query:{Style.RESET_ALL} ")
-        search(query)
+        search(query, "database/")
     elif command == "2":
         username = input(f"{Fore.RED}Enter Minecraft username:{Style.RESET_ALL} ")
         get_previous_usernames(username)
