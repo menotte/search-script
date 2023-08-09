5df3adedda4072500a53d7f59dd24756a5a7f276
menotte <139005962+menotte@users.noreply.github.com>
2023-08-09 23:30:01 +0200

    Update search.py

--- search.py
+++ search.py
@@ -30,31 +30,7 @@ def get_previous_usernames(username):
                     print(f"{Fore.RED}Usernames for {username}:{Style.RESET_ALL}")
                     for name in usernames:
                         print(f"{Fore.RED}Username:{Style.RESET_ALL} {name}")
-                        search(name, "minecraft/")
-                else:
-                    print("No usernames found for the user.")
-            else:
-                print("Failed to retrieve previous usernames.")
-        else:
-            print("No UUID found for the username.")
-    else:
-        print("Failed to retrieve UUID for the username.")
-def get_previous_usernames2(username):
-    command = f'curl -s "https://laby.net/api/search/names/{username}"'
-    response = os.popen(command).read()
-    if response:
-        data = json.loads(response)
-        if "results" in data and len(data["results"]) > 0:
-            uuid = data["results"][0]["uuid"]
-            command = f'curl -s "https://laby.net/api/user/{uuid}/get-names"'
-            response = os.popen(command).read()
-            if response:
-                usernames = [name["name"] for name in json.loads(response)]
-                if len(usernames) > 0:
-                    print(f"{Fore.RED}Usernames for {username}:{Style.RESET_ALL}")
-                    for name in usernames:
-                        print(f"{Fore.RED}Username:{Style.RESET_ALL} {name}")
-                        search(name, "wakanim/")
+                        search(name, "database/")
                 else:
                     print("No usernames found for the user.")
             else:
@@ -87,15 +63,13 @@ while True:
     print(f"[{Fore.GREEN}2{Style.RESET_ALL}] final")
     print(f"[{Fore.GREEN}3{Style.RESET_ALL}] wakanim")
     print(f"[{Fore.GREEN}4{Style.RESET_ALL}] facebook")
-    print(f"[{Fore.GREEN}5{Style.RESET_ALL}] final wakanim")
-
-    print(f"[{Fore.GREEN}6{Style.RESET_ALL}] exit")
+    print(f"[{Fore.GREEN}5{Style.RESET_ALL}] exit")
 
     command = input(f"\n{Fore.GREEN}Enter the command number:{Style.RESET_ALL} ")
 
     if command == "1":
         query = input(f"{Fore.RED}Enter search query:{Style.RESET_ALL} ")
-        search(query, "minecraft/")
+        search(query, "database/")
     elif command == "2":
         username = input(f"{Fore.RED}Enter Minecraft username:{Style.RESET_ALL} ")
         get_previous_usernames(username)
@@ -105,11 +79,8 @@ while True:
     elif command == "4":
         query = input(f"{Fore.RED}Enter Facebook search query:{Style.RESET_ALL} ")
         france_search(query)
-    elif command == "6":
+    elif command == "5":
         break
-    elif command =="5":
-        query = input(f"{Fore.RED}Enter Minecraft username:{Style.RESET_ALL} ")
-        get_previous_usernames2(query)
     else:
         print("Invalid command number. Please try again.")
 
